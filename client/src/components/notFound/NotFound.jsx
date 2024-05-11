84986530f7815a0e81e714de56e4c8544f547f6d
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-05-12 00:15:10 +0300

    Update not found page

--- client/src/components/notFound/NotFound.jsx
+++ client/src/components/notFound/NotFound.jsx
@@ -1,9 +1,22 @@
 import './NotFound.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NotFound() {
+
+    
     return (
-        <div>NotFound</div>
+        <>
+            <div className="not-found-ctr">
+                <h1>404</h1>
+                <div className='upper'>
+                    <h2>Page not found!</h2>
+                </div>
+                <div className='lower'>
+                    <Link to={'/'} className='btn'>Home page</Link>
+                </div>
+            </div>
+        </>
     )
 }
 
