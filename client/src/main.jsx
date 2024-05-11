3e7688eb9c127f253c897abe371fd3e3cb09ef91
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-05-12 00:16:27 +0300

    Provide search context to the app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,17 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from './contexts/ThemeContext.jsx';
+import { SearchProvider } from './contexts/SearchContext.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider> {/* DARK MODE CONTEXT */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <SearchProvider> {/* SEARCH VALUE CONTEXT */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SearchProvider>
   </ThemeProvider>
 );
