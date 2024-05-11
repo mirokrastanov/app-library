57c933525adce6ce9e7031645f74c29bcd008eef
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-05-12 00:14:52 +0300

    Update Home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,9 +1,23 @@
+import { Link } from 'react-router-dom';
 import './Home.css';
 import React from 'react';
 
 function Home() {
     return (
-        <div>Home</div>
+        <div className='home-ctr'>
+
+            <section className='h--section-top'>
+                <div className="h--content">
+                    <div className="h--info">
+                        <p>
+                            Enjoy!
+                        </p>
+                        <p>APP CARDS BELOW...</p>
+                        {/* <Link className='btn' to={'/shows'}>Shows</Link> */}
+                    </div>
+                </div>
+            </section>
+        </div>
     )
 }
 
