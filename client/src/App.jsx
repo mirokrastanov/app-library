b635fb3b789ba053de4e17fe4b19de36185aa793
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-05-11 23:25:18 +0300

    Incorporate current elements and context into the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { Routes, Route, Navigate, useParams, useLocation } from 'react-router-dom';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { useTheme } from './contexts/ThemeContext';
+import Navigation from './components/navigation/Navigation';
+import Home from './components/home/Home';
+import NotFound from './components/notFound/NotFound';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const darkTheme = useTheme();
+    // const location = useLocation();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // useEffect(() => {
+    //     console.log(location);
+    // }, [location]);
+
+    return (
+        <div className={`app-ctr${darkTheme ? ' dark-mode' : ''}`}>
+
+            <Navigation />
+
+            <div className="app-content">
+                <Routes>
+                    <Route exact path='/' element={<Home />} />
+                    <Route path='home' element={<Navigate to='/' />} />
+                    <Route path='index' element={<Navigate to='/' />} />
+                    
+
+
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+            </div>
+
+        </div>
+    )
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from './contexts/ThemeContext.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <ThemeProvider> {/* DARK MODE CONTEXT */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>
+);
