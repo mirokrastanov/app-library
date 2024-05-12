b0db07061513fd1f8e6af97288ef8a21b4a9f035
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-05-13 00:17:55 +0300

    Update structure

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -5,13 +5,10 @@ import React from 'react';
 function Home() {
     return (
         <div className='home-ctr'>
-
             <section className='h--section-top'>
+                <h1 className='h--title'>Apps & Games</h1>
                 <div className="h--content">
                     <div className="h--info">
-                        <p>
-                            Enjoy!
-                        </p>
                         <p>APP CARDS BELOW...</p>
                         {/* <Link className='btn' to={'/shows'}>Shows</Link> */}
                     </div>
