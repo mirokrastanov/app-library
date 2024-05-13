08956df08fdb22d04ad765bfcdd44dc14752ecf7
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-05-13 18:18:53 +0300

    Create rps component

---
+++ client/src/components/apps/rps/RockPaperScissors.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './RockPaperScissors.css';
+
+function RockPaperScissors() {
+
+
+
+
+    return (
+        <div>RockPaperScissors</div>
+    )
+}
+
+export default RockPaperScissors;
