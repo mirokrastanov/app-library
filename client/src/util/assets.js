827bf5729e4f7a3dfc72f77d720448d4407edf9b
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-05-13 17:40:51 +0300

    Add tech img urls

---
+++ client/src/util/assets.js
@@ -0,0 +1,16 @@
+export const techImgUrl = {
+    'HTML': 'https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/HTML.svg',
+    'CSS': 'https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/CSS.svg',
+    'JavaScript': 'https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/JavaScript.svg',
+    'lit-html': 'https://raw.githubusercontent.com/tandpfun/skill-icons/main/icons/Lit-Dark.svg',
+    'Page.js': 'https://raw.githubusercontent.com/mirokrastanov/Software-Engineering-SoftUni/main/miscellaneous/page-js-1.png',
+    'Axios': 'https://avatars.githubusercontent.com/u/32372333?v=4',
+    'Angular': 'https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/angularjs/angularjs-original.svg',
+    'TypeScript': 'https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/typescript/typescript-original.svg',
+    'Firebase': 'https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/firebase/firebase-plain.svg',
+    'Puppeteer': 'https://github.com/mirokrastanov/Software-Engineering-SoftUni/blob/main/miscellaneous/puppeteer-1.png?raw=true',
+    'React': 'https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/react/react-original.svg',
+    'Appwrite': 'https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/appwrite/appwrite-original.svg',
+    
+};
+
