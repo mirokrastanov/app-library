20ff73ccd544ee671ec85aa15c5ab74b48520be9
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-05-15 23:28:29 +0300

    Game complete

--- client/src/apps/rps/RockPaperScissors.jsx
+++ client/src/apps/rps/RockPaperScissors.jsx
@@ -7,14 +7,18 @@ function RockPaperScissors() {
     const [playerChoice, setPlayerChoice] = useState('');
     const [computerChoice, setComputerChoice] = useState('');
     const [currentHover, setCurrentHover] = useState('');
+    const [winner, setWinner] = useState('');
     const isRock = currentHover === 'r';
     const isPaper = currentHover === 'p';
     const isScissors = currentHover === 's';
     const navigate = useNavigate();
 
-    // useEffect(() => {
-    //     console.log(currentHover);
-    // }, [currentHover])
+    useEffect(() => {
+        if (playerChoice !== '' && computerChoice !== '') {
+            checkWinner();
+            setGameOver(true);
+        }
+    }, [computerChoice])
 
     function onMouseOver(e) { setCurrentHover(e.target.dataset.choice) }
     function onMouseLeave(e) { setCurrentHover('') }
@@ -22,7 +26,6 @@ function RockPaperScissors() {
         if (e.target.dataset.choice === 'r' || e.target.dataset.choice === 'p' || e.target.dataset.choice === 's') {
             setPlayerChoice(e.target.dataset.choice);
             setComputerChoice(genComputerChoice());
-            setGameOver(true);
         }
     }
     function resetGame() {
@@ -30,16 +33,33 @@ function RockPaperScissors() {
         setPlayerChoice('');
         setComputerChoice('');
         setCurrentHover('');
+        setWinner('');
     }
     function genComputerChoice() {
         const choices = ['r', 'p', 's'];
         return choices[Math.floor(Math.random() * 3)];
+    }
+    function checkWinner() {
+        // console.log(playerChoice, ' | ', computerChoice);
+        if (playerChoice === computerChoice) {
+            setWinner('It\'s a tie');
+        } else if (playerChoice === 'r' && computerChoice === 's') {
+            setWinner('You win');
+        } else if (playerChoice === 'p' && computerChoice === 'r') {
+            setWinner('You win');
+        } else if (playerChoice === 's' && computerChoice === 'p') {
+            setWinner('You win');
+        } else {
+            setWinner('You lose');
+        }
     }
 
     return (
         <div className='app-demo-ctr'>
             <h1>APP DEMO</h1>
             <div style={{ margin: '10px 10px 30px 10px' }} className='btn' onClick={() => navigate(-1)}>Go Back</div>
+
+
             <h2 id="choice" className={gameOver ? 'el-hidden' : ''}>Make your choice</h2>
             <div id="select" className={gameOver ? 'el-hidden' : ''}>
                 <img src="/rps/rock.png" alt="rock-img"
@@ -52,7 +72,9 @@ function RockPaperScissors() {
                     onMouseOver={onMouseOver} onMouseLeave={onMouseLeave} data-choice="s"
                     className={isPaper || isRock ? 'op-50' : ''} onClick={onChoiceSelected} />
             </div>
-            <h1 id="win" className={!gameOver ? 'el-hidden' : ''}>YOU WIN</h1>
+
+
+            <h1 id="win" className={!gameOver ? 'el-hidden' : ''}>{winner}</h1>
             <div id="score" className={!gameOver ? 'el-hidden' : ''}>
                 <div>
                     <h2 id="you">YOU</h2>
@@ -67,8 +89,7 @@ function RockPaperScissors() {
                     {computerChoice === 's' && (<img src="/rps/scissors.png" alt="scissors-img" />)}
                 </div>
             </div>
-            <div className={`btn${!gameOver ? ' el-hidden' : ''}`}
-                onClick={resetGame} >Play Again</div>
+            <div className={`btn${!gameOver ? ' el-hidden' : ''}`} onClick={resetGame} >Play Again</div>
         </div>
     )
 }
