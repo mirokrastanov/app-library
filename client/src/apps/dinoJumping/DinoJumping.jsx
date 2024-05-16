eebd770c9040ca05df1a030969645ded1ddb7fb5
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-05-16 22:39:02 +0300

    Initial Structure and Design + animations

--- client/src/apps/dinoJumping/DinoJumping.jsx
+++ client/src/apps/dinoJumping/DinoJumping.jsx
@@ -3,7 +3,13 @@ import './DinoJumping.css';
 
 function DinoJumping() {
     return (
-        <div>DinoJumping</div>
+        <div className="app-demo-ctr" id="dino">
+            <div className="game">
+                <div id="character"></div>
+                <div id="block"></div>
+            </div>
+            <p>Score: <span id="scoreSpan"></span></p>
+        </div>
     )
 }
 
