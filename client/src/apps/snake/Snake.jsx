7851b69e2434043f0d0c53c9b19b97833a0a8e6d
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-05-17 16:21:57 +0300

    Initial structure and design

--- client/src/apps/snake/Snake.jsx
+++ client/src/apps/snake/Snake.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import './Snake.css';
+import React, { useEffect, useRef, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Snake() {
+    const [counter, setCounter] = useState(0);
+    const [gameEnded, setGameEnded] = useState(false);
+    const character = useRef(null);
+    const block = useRef(null);
+    const gameBoard = useRef(null);
+    const navigate = useNavigate();
+
     return (
-        <div>Snake</div>
+        <div className="app-demo-ctr" id="dino">
+            <h1>Snake Demo</h1>
+            <div id="snake-board">
+                <div id="snake-score">5000</div>
+                <canvas id="canvas" width="600" height="600"></canvas>
+
+                <div id="end-btn">Play Again</div>
+            </div>
+
+        </div>
     )
 }
 
