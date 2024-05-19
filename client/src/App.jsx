d4b703e6345ff22101d35be8b6edd32b0371bf8c
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2024-05-19 14:31:24 +0300

    Add route for breakout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import RockPaperScissors from './apps/rps/RockPaperScissors';
 import GuessTheNumber from './apps/guess/GuessTheNumber';
 import DinoJumping from './apps/dinoJumping/DinoJumping';
 import Snake from './apps/snake/Snake';
+import Breakout from './apps/breakout/Breakout';
 
 function App() {
     const darkTheme = useTheme();
@@ -35,6 +36,7 @@ function App() {
                     <Route path='/apps/guess' element={<GuessTheNumber />} />
                     <Route path='/apps/dino-jumping' element={<DinoJumping />} />
                     <Route path='/apps/snake' element={<Snake />} />
+                    <Route path='/apps/breakout' element={<Breakout />} />
 
 
 
